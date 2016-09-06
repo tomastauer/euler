15c7a12a58919bc7492d7b756854b0d63c040d66
Tomas <tauer.t@gmail.com>
2016-09-06 19:22:19 +0200

    added tom's solution

--- Euler.Solver/Tom/EvenFibonacciNumbers.cs
+++ Euler.Solver/Tom/EvenFibonacciNumbers.cs
@@ -4,7 +4,19 @@
     {
         public long SolveProblem(int input)
         {
-            return 0;
+            int previous = 0;
+            int actual = 2;
+            long result = 0;
+
+            while (actual <= input)
+            {
+                result += actual;
+                int tmp = actual;
+                actual = actual * 4 + previous;
+                previous = tmp;
+            }
+
+            return result;
         }
     }
 }
