0c53fc2293d79d4e53db80d4edef338fa1cf137c
Tomas <tauer.t@gmail.com>
2016-09-13 21:07:00 +0200

    solution

--- Euler.Solver/Tom/PrimeFactors.cs
+++ Euler.Solver/Tom/PrimeFactors.cs
@@ -1,10 +1,51 @@
-﻿namespace Euler.Solver.Tom
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler.Solver.Tom
 {
     public class PrimeFactors : IScalarProblemSolver
     {
+        private bool[] mPrimes;
+        private int l = 6900;
+        private int counter;
+
         public long SolveProblem(long input)
         {
-            return 0;
+            mPrimes = new bool[l];
+            counter = 2;
+            long tmp = input;
+
+            while (true)
+            {
+                var currentPrime = GetNextPrime();
+                while (tmp % currentPrime == 0)
+                {
+                    tmp /= currentPrime;
+                }
+                
+                if (tmp == 1)
+                {
+                    return currentPrime;
+                }
+            }
+        }
+
+        public int GetNextPrime()
+        {
+            while (true)
+            {
+                if (!mPrimes[counter])
+                {
+                    for (int j = 1; j * counter < l; j++)
+                    {
+                        mPrimes[j * counter] = true;
+                    }
+                    return counter++;
+                }
+
+                counter++;
+            }
         }
     }
 }
