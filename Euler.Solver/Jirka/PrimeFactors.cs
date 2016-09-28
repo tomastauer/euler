0888fac336ec52dc491ca02766c0ed24c391c96d
jiris2 <jiris2@kentico.com>
2016-09-28 15:44:35 +0200

    Prime factors (Euler 3) by Jirka

--- Euler.Solver/Jirka/PrimeFactors.cs
+++ Euler.Solver/Jirka/PrimeFactors.cs
@@ -1,12 +1,51 @@
-﻿namespace Euler.Solver.Jirka
+﻿using System.Collections.Generic;
+
+namespace Euler.Solver.Jirka
 {
     public class PrimeFactors : IScalarProblemSolver
     {
         public long SolveProblem(long input)
         {
+            return BruteForce(input);
+        }
+
+        private long BruteForce(long input)
+        {
+            while (true)
+            {
+                if (input%2 == 0)
+                {
+                    input = input/2;
+                    if (input == 1)
+                    {
+                        return 2;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
 
 
-            return 0;
+            int number = 3;
+            while (true)
+            {
+                if (input%number == 0)
+                {
+                    input = input/number;
+                    if (input == 1)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    number += 2;
+                }
+            }
+
+            return number;
         }
     }
 }
