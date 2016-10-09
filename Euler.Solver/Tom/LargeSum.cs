d0f20bf387d9f2f8ef4b1db220005271abb60e53
Tomas <tauer.t@gmail.com>
2016-10-09 22:09:26 +0200

    Large sum reseni

--- Euler.Solver/Tom/LargeSum.cs
+++ Euler.Solver/Tom/LargeSum.cs
@@ -1,16 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Euler.Solver.Tom
+﻿namespace Euler.Solver.Tom
 {
     public class LargeSum : IStringArrayInputProblemSolver
     {
+        private const int ASCII_NUMBER = 48;
+
         public long SolveProblem(string[] input)
         {
-            throw new NotImplementedException();
+            int lineLength = input[0].Length;
+            int reminder = 0;
+
+            string result = null;
+
+            int asciiSubtractor = input.Length * ASCII_NUMBER;
+
+            for (int i = lineLength - 1; i >= 0; i--)
+            {
+                int interResult = 0;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    interResult += input[j][i];
+                }
+
+                interResult += reminder - asciiSubtractor;
+                int lastDigit = interResult % 10;
+                result = lastDigit + result;
+
+                reminder = (interResult - lastDigit) / 10;
+            }
+
+            if (reminder != 0)
+            {
+                result = reminder + result;
+            }
+
+            var resultString = result;
+            return long.Parse(resultString.Length > 10 ? resultString.Substring(0, 10) : resultString);
         }
     }
 }
--- Euler.Tests/Euler.Tests/LargeSumTests.cs
+++ Euler.Tests/Euler.Tests/LargeSumTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 using NUnit.Framework;
 
@@ -7,7 +8,7 @@ namespace Euler.Tests
     [TestFixture]
     public class LargeSumTests : StringArrayInputProblemTest
     {
-        private const string ASSETS_PATH = "Assets/LargeSum/";
+        private readonly string mAssetsPath = AppDomain.CurrentDomain.BaseDirectory + "/Assets/LargeSum/";
 
         public LargeSumTests() : base(
             tomsSolver : new Solver.Tom.LargeSum(), 
@@ -16,43 +17,44 @@ namespace Euler.Tests
         }
 
 
-        [TestCase("Euler.txt", 0)]
+        [TestCase("Euler.txt", 5537376230)]
         [TestCase("Test1.txt", 9)]
         [TestCase("Test2.txt", 15)]
         [TestCase("Test3.txt", 253421)]
         [TestCase("Test4.txt", 1234567890)]
-        public void CorrectnessTom(string file, int expectedResult)
+        public void CorrectnessTom(string file, long expectedResult)
         {
-            var input = File.ReadAllLines(Path.Combine(ASSETS_PATH, file));
+            var input = File.ReadAllLines(Path.Combine(mAssetsPath, file));
             CorrectnessBaseTom(input, expectedResult);
         }
 
 
-        [TestCase("Euler.txt", 0)]
+        [TestCase("Euler.txt", 5537376230)]
         [TestCase("Test1.txt", 9)]
         [TestCase("Test2.txt", 15)]
         [TestCase("Test3.txt", 253421)]
         [TestCase("Test4.txt", 1234567890)]
-        public void CorrectnessJirka(string file, int expectedResult)
+        public void CorrectnessJirka(string file, long expectedResult)
         {
-            var input = File.ReadAllLines(Path.Combine(ASSETS_PATH, file));
+            var input = File.ReadAllLines(Path.Combine(mAssetsPath, file));
             CorrectnessBaseTom(input, expectedResult);
         }
 
 
         [Test]
+        [Repeat(3)]
         public void PerformanceTom()
         {
-            var input = File.ReadAllLines(Path.Combine(ASSETS_PATH, "Euler.txt"));
-            PerformanceBaseTom(input, 10000000);
+            var input = File.ReadAllLines(Path.Combine(mAssetsPath, "Euler.txt"));
+            PerformanceBaseTom(input, 1000);
         }
 
 
         [Test]
         public void PerformanceJirka()
         {
-            var input = File.ReadAllLines(Path.Combine(ASSETS_PATH, "Euler.txt"));
-            PerformanceBaseJirka(input, 10000000);
+            var input = File.ReadAllLines(Path.Combine(mAssetsPath, "Euler.txt"));
+            PerformanceBaseJirka(input, 1);
         }
     }
 }
